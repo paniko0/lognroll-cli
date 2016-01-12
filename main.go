9e9bbe46ffc93fff3894c5817705764628a8cf13
Danillo Souza <danillo.fs@gmail.com>
2016-01-12 09:32:12 -0200

    connecting to pubnub and adding some flags to authenticate and setup log file

--- main.go
+++ main.go
@@ -1,41 +1,97 @@
 package main
 
 import (
-	"github.com/howeyc/fsnotify"
-	"io/ioutil"
+	"flag"
+	"fmt"
+	"github.com/hpcloud/tail"
+	"github.com/pubnub/go/messaging"
 	"log"
+	"time"
+)
+
+var (
+	logFile      string
+	pubKey       string
+	subscribeKey string
+	secretKey    string
+	channelName  string
 )
 
 func main() {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	initVars()
 
 	done := make(chan bool)
 
 	// Process events
 	go func() {
-		for {
-			ev := <-watcher.Event
-			log.Println(ev)
-			if ev.IsModify() {
-				bb, err := ioutil.ReadFile(ev.Name)
-				if nil != err {
-					return
-				}
-				log.Println("new contents:", string(bb))
-			}
+
+		t, err := tail.TailFile(logFile, tail.Config{Follow: true})
+		for line := range t.Lines {
+			log.Println(line.Text)
+
+			pubInstance := messaging.NewPubnub(pubKey, subscribeKey, secretKey, "", false, channelName)
+
+			var errorChannel = make(chan []byte)
+			var callbackChannel = make(chan []byte)
+			go pubInstance.Publish(channelName, line.Text, callbackChannel, errorChannel)
+			go handleResult(callbackChannel, errorChannel, 1000, "Publish")
+			// please goto the top of this file see the implementation of handleResult
+
+		}
+
+		if nil != err {
+			return
 		}
 	}()
-
-	err = watcher.Watch("/Users/danillosouza/dev/go/src/github.com/paniko0/lognroll-cli/teste.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Hang so program doesn't exit
 	<-done
 
-	watcher.Close()
+}
+
+func handleResult(successChannel, errorChannel chan []byte, timeoutVal int64, action string) {
+	timeout := make(chan bool, 1)
+	go func() {
+		time.Sleep(time.Duration(timeoutVal) * time.Second)
+		timeout <- true
+	}()
+	for {
+		select {
+		case success, ok := <-successChannel:
+			if !ok {
+				break
+			}
+			if string(success) != "[]" {
+				log.Println(fmt.Sprintf("%s Response: %s ", action, success))
+				log.Println("")
+			}
+			return
+		case failure, ok := <-errorChannel:
+			if !ok {
+				break
+			}
+			if string(failure) != "[]" {
+				// if displayError {
+				// 	log.Println(fmt.Sprintf("%s Error Callback: %s", action, failure))
+				// 	log.Println("")
+				// }
+			}
+			return
+		case <-timeout:
+			log.Println(fmt.Sprintf("%s Handler timeout after %d secs", action, timeoutVal))
+			log.Println("")
+			return
+		}
+	}
+}
+
+func initVars() {
+	flag.StringVar(&pubKey, "pubKey", "", "pubnub public key")
+	flag.StringVar(&subscribeKey, "subscribeKey", "", "pubnub subscribe key")
+	flag.StringVar(&secretKey, "secretKey", "", "pubnub secret key")
+	flag.StringVar(&logFile, "logFile", "/var/logs/error.log", "complete path to log")
+	flag.StringVar(&channelName, "channelName", "", "channel name to connect to")
+
+	flag.Parse()
 }
